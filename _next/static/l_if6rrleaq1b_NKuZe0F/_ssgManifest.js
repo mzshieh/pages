091f188e0b527463636e8761f1842d020ebf420c
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2025-12-18 20:35:16 +0800

    adding base path

---
+++ _next/static/l_if6rrleaq1b_NKuZe0F/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fchampionship\u002F[year]","\u002Fchampionship\u002F[year]\u002F[section]","\u002Fchampionship\u002F[year]\u002Fchangelogs","\u002Fchampionship\u002Flatest\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
